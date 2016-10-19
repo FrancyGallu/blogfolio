36b796668b77651914e9d65328c8c7cef5be162f
FrancyGallu <galluzzi.francesca@gmail.com>
2016-10-19 15:52:43 +0100

    User management system implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :tags
-  devise_for :users
   resources :posts
+
+  devise_for :users, controllers: {
+      sessions: 'users/sessions'
+  }
 
   root 'posts#index'
 
